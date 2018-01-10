96232c85cd25567738c25fa9b2ce24f8b352fe12
go2santosh <go2santosh@hotmail.com>
2018-01-10 16:21:01 -0600

    Update swiftToJava.js

--- swiftToJava.js
+++ swiftToJava.js
@@ -1,77 +1,84 @@
+
+
 function convertSwiftToJava(swiftCode) {
-	
-    var swiftLines = swiftCode.split("\n");
-    var javaLines = [];
-    for (i = 0; i < swiftLines.length; i++) {
-    	var swiftLine = swiftLines[i].trim();
-        var javaLine;
-    	if (swiftLine.startsWith("static let ")) {
-        	javaLine = convertSwiftConstantDeclarationToJava(swiftLine);
-        }
-        else {
-        	javaLine = "//TODO: ".concat(swiftLine);
-        }
-        javaLine = addSemicollonIfMissing(javaLine);
-        javaLines.push(javaLine);
+  var swiftLines = swiftCode.split("\n");
+  var javaLines = [];
+  for (i = 0; i < swiftLines.length; i++) {
+    var swiftLine = swiftLines[i].trim();
+    var javaLine;
+    if (swiftLine.startsWith("static let ")) {
+      javaLine = convertSwiftConstantDeclarationToJava(swiftLine);
     }
-    return javaCode = javaLines.join("\n");
+    else if (swiftLine.startsWith("//")) {
+      javaLine = swiftLine;
+    }
+    else if (swiftLine.length == 0) {
+      javaLine = swiftLine;
+    }
+    else {
+      javaLine = "//TODO: ".concat(swiftLine);
+    }
+    javaLines.push(javaLine);
+  }
+  return javaCode = javaLines.join("\n");
 }
 
 function convertSwiftConstantDeclarationToJava(swiftStatement) {
-	
-    var success = true;
-    var javaStatement;
-    
-    if (swiftStatement.startsWith("static let ")) {
-    
-        var remainingSwiftStatement = swiftStatement;
-        var javaDeclarationStart;
 
-    	javaDeclarationStart = "private static final";
-        remainingSwiftStatement = remainingSwiftStatement.substring("static let ".length);
-        
-        var variableNameAndValue = remainingSwiftStatement.split("=");
-        var variableNameAndType = variableNameAndValue[0].split(":");
-        var variableName = variableNameAndType[0].trim();
-        var variableValue = variableNameAndValue[1].trim();
-        var variableSwiftType = "";
-        if (variableNameAndType.length > 1) {
-        	variableSwiftType = variableNameAndType[1].trim();
-        }
-        else {
-        	if (variableValue.startsWith("\"")) {
-            	variableSwiftType = "String";
-            }
-            else {
-            	success = false;
-            }
-        }
-        
-        javaStatement = javaDeclarationStart
-          .concat(" ")
-          .concat(variableSwiftType)
-          .concat(" ")
-          .concat(variableName)
-          .concat(" = ")
-          .concat(variableValue);
+  var success = true;
+  var javaStatement;
+
+  if (swiftStatement.startsWith("static let ")) {
+
+    var remainingSwiftStatement = swiftStatement;
+    var javaDeclarationStart;
+
+    javaDeclarationStart = "private static final";
+    remainingSwiftStatement = remainingSwiftStatement.substring("static let ".length);
+
+    var variableNameAndValue = remainingSwiftStatement.split("=");
+    var variableNameAndType = variableNameAndValue[0].split(":");
+    var variableName = variableNameAndType[0].trim();
+    var variableValue = variableNameAndValue[1].trim();
+    var variableSwiftType = "";
+    if (variableNameAndType.length > 1) {
+      variableSwiftType = variableNameAndType[1].trim();
     }
     else {
-    	success = false;
+      if (variableValue.startsWith("\"")) {
+        variableSwiftType = "String";
+      }
+      else {
+        success = false;
+      }
     }
-    
-    if (success) {
-    	return javaStatement;
-    }
-    else {
-    	return "//TODO: ".concat(swiftStatement);
-    }
+
+    javaStatement = javaDeclarationStart
+      .concat(" ")
+      .concat(variableSwiftType)
+      .concat(" ")
+      .concat(variableName)
+      .concat(" = ")
+      .concat(variableValue)
+      .concat(";");
+  }
+  else {
+    success = false;
+  }
+
+  if (success) {
+    return javaStatement;
+  }
+  else {
+    return "//TODO: ".concat(swiftStatement);
+  }
 }
 
 function addSemicollonIfMissing(text) {
 
-    var updatedStatement = text;
-    if (updatedStatement.slice(-1) != ';') {
-    	updatedStatement = updatedStatement.concat(";");
-    }
-    return updatedStatement;
+  var updatedStatement = text;
+  if (updatedStatement.slice(-1) != ';') {
+    updatedStatement = updatedStatement.concat(";");
+  }
+  return updatedStatement;
 }
